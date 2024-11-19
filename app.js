47f31d59054b84d5d22a2519518e1865d7c43586
unknown <windikboson@gmail.com>
2024-11-20 04:29:33 +0900

    belajar request mengunaka api dari libray axios

---
+++ app.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('#search-form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    document.querySelectorAll('img').forEach((img) => img.remove());
+
+    const keyword = form.elements.query.value;
+    const config = {
+        params : { q : keyword},
+    }
+    
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    getImage(res.data);
+    form.elements.query.value = '';
+});
+
+const getImage = (shows) => {
+    for (let result of shows){
+        if (result.show.image){
+            const img = document.createElement('img');
+            const container =document.querySelector('#mycontainer')
+            img.src = result.show.image.medium
+           container.append(img);
+        }
+    }
+}
